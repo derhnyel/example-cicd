a20512899eeba8043b76448633ed16145302c4af
derhnyel <ejedenials@gmail.com>
2022-05-21 00:06:14 +0100

    Initial commit

---
+++ .circleci/send_text_function.py
@@ -0,0 +1,30 @@
+import os
+import json
+import boto3
+
+sns_client = boto3.client('sns')
+
+def lambda_handler(event, context):
+
+  text_message = "Congrats! You deployed this application using a CI/CD pipeline."
+
+  phone_number = os.environ['MY_PHONE_NUMBER']
+
+  print(f"Publishing message to SNS topic. Subscriber phone number ends in {phone_number[-4:]}")
+  
+  try:
+    publish_sns_update(text_message)
+    print("Message send success.")
+  except Exception as error:
+    print(f"Message send failed, error: {error}")
+
+def publish_sns_update(text_message):
+
+  response = sns_client.publish(
+      TargetArn = os.environ['SNS_TOPIC_ARN'], 
+      Message=json.dumps({'default': text_message}),
+      MessageStructure='json'
+  )
+
+  return response
+#aws s3api create-bucket --bucket serverless-cicd-example --region us-east-1
